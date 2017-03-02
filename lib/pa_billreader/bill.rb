124de41f4c6fd44df2a1816815a02d81c459e06d
Kelsey Huse <kelseyhuse30@gmail.com>
2017-03-01 20:15:07 -0500

    creating bill object in scrape method

--- lib/pa_billreader/bill.rb
+++ lib/pa_billreader/bill.rb
@@ -3,9 +3,6 @@ class PaBillreader::Bill
 	attr_accessor :name, :number, :topic, :sponsor, :last_action, :full_text
 
 	def self.all
-		#scrape house and senate and return bills based on that data
-		# PaBillreader::BillScraper.new("http://www.legis.state.pa.us/cfdocs/legis/bi/BillIndx.cfm?sYear=2017&sIndex=0&bod=H")
-		# PaBillreader::BillScraper.new("http://www.legis.state.pa.us/cfdocs/legis/bi/BillIndx.cfm?sYear=2017&sIndex=0&bod=S")
 
 		self.scrape_bills
 
@@ -16,10 +13,21 @@ class PaBillreader::Bill
 		#go to senate, find bill
 		#extract properties
 		#instantiate a bill
-
+		bills << self.scrape_senate
+		bills << self.scrape_house
 		
 		bills
 
 	end
 
+	def self.scrape_house
+		doc = Nokogiri::HTML(open("http://www.legis.state.pa.us/cfdocs/legis/bi/BillIndx.cfm?sYear=2017&sIndex=0&bod=H"))
+		number = doc.css()
+		bill = self.new
+		bill.number = number
+	end
+
+	def self.scrape_senate
+		doc = Nokogiri::HTML(open("http://www.legis.state.pa.us/cfdocs/legis/bi/BillIndx.cfm?sYear=2017&sIndex=0&bod=S"))
+	end
 end
