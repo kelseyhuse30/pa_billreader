017a67a5344c96af36fbb5cfd6cbb40f4a99dac9
Kelsey Huse <kelseyhuse30@gmail.com>
2017-03-01 19:23:36 -0500

    created loop for menu and selection

--- lib/pa_billreader/cli.rb
+++ lib/pa_billreader/cli.rb
@@ -3,6 +3,8 @@ class PaBillreader::CLI
 	def call
 		puts "Would you like to view Senate or House bills?"
 		list_bills
+		menu
+		goodbye
 	end
 
 	def list_bills
@@ -10,6 +12,28 @@ class PaBillreader::CLI
 		13. An Act amending Title 18 (Crimes and Offenses) of the Pennsylvania Consolidated Statutes, in riot, disorderly conduct and related offenses, further providing for the offense of cruelty to animals.
 		14. An Act amending Title 24 (Education) of the Pennsylvania Consolidated Statutes, providing for the role of institutions of higher education in immigration enforcement, for restrictions on immigration policies and for duties of the Department of Education.
 	DOC
+	end
+
+	def menu
+		input = nil
+		while input != "exit"
+			puts "Enter the number of the bill you'd like to view, or type list to see bills again, or type exit"
+			input = gets.strip.downcase
+			case input
+			when "13"
+				puts "more info on bill 13..."
+			when "14"
+				puts "more info on bill 14..."
+			when "list"
+				list_bills
+			else
+				puts "Not sure what you want, type list or exit"
+			end
+		end
+	end
+
+	def goodbye
+		puts "See you later for more bills!"
 	end
 
 end
